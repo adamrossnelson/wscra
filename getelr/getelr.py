8dead436b2b9be8f6a793913a8256b582c83efac
Adam Ross Nelson <adamrossnelson@users.noreply.github.com>
2018-01-27 14:00:28 -0600

    End of day
    End of day

--- getelr/getelr.py
+++ getelr/getelr.py
@@ -1,27 +1,54 @@
 # File grab education law reporter.
 # https://educationlaw.org/member-services/members-pages/slr-back-issues
 
+from time import sleep
 from selenium import webdriver
 import os
+import requests
 
-fp = webdriver.FirefoxProfile()
-fp.set_preference("browser.download.folderList",2)
-fp.set_preference("browser.download.manager.showWhenStarting",False)
-fp.set_preference("browser.download.dir", os.getcwd())
-fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
+browser = webdriver.Firefox()
 
-browser = webdriver.Firefox(firefox_profile=fp)
-
+# Get webpage. Send user to browser to enter credentials.
 browser.get('https://educationlaw.org/member-services/members-pages/slr-back-issues')
-
-print('Visit browser, log in using your credentials, press enter here when ready to continue. ', end='')
+print('\n\n\n Visit browser, log in using your credentials, press enter here when ready to continue. ', end='')
 discarded_wait = input()
 print('\n\n\n')
 
+# Declare year index. Will count backwards from 2017.
+year = 2017
 elems = browser.find_elements_by_link_text('Read more ...')
-for e in len(elems):
+# Would prefer e in elems suntax...
+# Needs to use range(len(elems)) syntax because after returning at end of loop
+# list of elems needs a refresh. Otherwise stale error occurs.
+for e in range(len(elems)):
+    elems = browser.find_elements_by_link_text('Read more ...')
+    print('STARTING ' + str(year))
     elems[e].click()
-    issues = browser.find_elements_by_css_selector('.article-content > p:nth-child(2) > span:nth-child(1) > a:nth-child(1)')
-    for i in len(issues):
-        issues[i].click()
+    sleep(.5)
+    # CSS Selector would be preferred but did not reliably identify desired links.
+    for m in [r'January & February','January','February','March','April','May','June','July','August','September','October','November','December']:
+        # Try/Except flow control; Some years some months not available.
+        try:
+            issue = browser.find_element_by_link_text(m)
+            filelink = issue.get_attribute('href')
+            r = requests.get(filelink)
+            sleep(2)
+            m1 = m.replace(r' &','')
+            m2 = m1 + str(year) + '.pdf'
+            print('Proceeding with ' + m2 + '.')
+            open(m2, 'wb').write(r.content)
+            sleep(1)
+        except:
+            print('No issue with link ' + m + ' name. Proceeding to next.')
+            sleep(1)
+    # Send browser back to original url.
     browser.back()
+    # Wait to ensure browswer loads.
+    sleep(1)
+    # Increment year index.
+    year = year - 1
+    # Give user feedback.
+    print('FINISHED ' + str(year))
+    print('  ')
+    print('  ')
+
